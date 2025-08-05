c237b778acdf0d034bdfd58d0e245fc6f24f272c
Migs <75819835+Miggy0428@users.noreply.github.com>
2025-08-06 02:43:13 +0800

    Add .next/static/chunks/src_app_municipal_page_tsx_8a7bc99f._.js 1754419392104

---
+++ .next/static/chunks/src_app_municipal_page_tsx_8a7bc99f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d80abfd6._.js",
+  "static/chunks/node_modules_061b910d._.js"
+],
+    source: "dynamic"
+});
